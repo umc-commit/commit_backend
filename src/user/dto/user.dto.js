4ae7b4617997f9e81dbdb9c13dcd0c89690a8468
taeyoung0524 <miseoseo1@naver.com>
2025-07-09 15:56:48 +0900

    FEAT : 회원가입요청 Dto 작성

---
+++ src/user/dto/user.dto.js
@@ -0,0 +1,9 @@
+export class UserSignupRequestDto {
+    constructor({nickname,provider, oauth_id, categories, agreements}) {
+        this.nickname = nickname;
+        this.provider = provider;
+        this.oauth_id = oauth_id;
+        this.categories = categories; // 배열
+        this.agreements = agreements; // 배열
+    }
+}
