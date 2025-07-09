9947b727ef09b1585b3a551939faeb9198074259
taeyoung0524 <miseoseo1@naver.com>
2025-07-09 23:19:07 +0900

    FIX : 사용자 error 메세지 수정

--- src/common/errors/user.errors.js
+++ src/common/errors/user.errors.js
@@ -5,7 +5,7 @@ export class OauthIdAlreadyExistError extends BaseError {
   constructor(data=null){
     super({
       errorCode:"U001",
-      reason : '이미 소셜로그인 계정이 존재합니다.',
+      reason : '이미 등록된 소셜 로그인 계정입니다.',
       statusCode: 409,
       data,
     })
