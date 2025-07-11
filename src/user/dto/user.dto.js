2b81e76b184e753bcc3da56b92dc3fee9342523f
taeyoung0524 <miseoseo1@naver.com>
2025-07-11 14:33:54 +0900

    feat : 로그인 요청 DTO 작성

--- src/user/dto/user.dto.js
+++ src/user/dto/user.dto.js
@@ -1,3 +1,5 @@
+
+// 회원가입 요청
 export class UserSignupRequestDto {
     constructor({nickname,provider, oauth_id, categories, agreements}) {
         this.nickname = nickname;
@@ -5,5 +7,13 @@ export class UserSignupRequestDto {
         this.oauth_id = oauth_id;
         this.categories = categories; // 배열
         this.agreements = agreements; // 배열
+    }
+}
+
+// 로그인 요청 
+export class UserLoginRequestDto{
+    constructor({provider, oauth_id}) {
+        this.provider = provider;
+        this.oauth_id = oauth_id;
     }
 }
