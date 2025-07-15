029ccf423c0a99b17a3e267d2620326737455001
taeyoung0524 <miseoseo1@naver.com>
2025-07-15 13:28:45 +0900

    FEAT : 네이버 passport 설치

--- src/auth.config.js
+++ src/auth.config.js
@@ -71,3 +71,38 @@ const kakaoVerify = async (profile) => {
     oauth_id : profile.id,
   };
 };
+
+
+export const naverStrategy = new KakaoStrategy(
+  {
+    clientID: process.env.PASSPORT_KAKAO_CLIENT_ID,
+    clientSecret: process.env.PASSPORT_KAKAO_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/api/users/oauth2/callback/kakao",
+    scope: undefined,
+    state: true,
+  },
+  (accessToken, refreshToken, profile, cb) => {
+    return kakaoVerify(profile)
+      .then((user) => cb(null, user))
+      .catch((err) => cb(err));
+  }
+);
+
+const naverVerify = async (profile) => {
+
+  const user = await prisma.account.findFirst({ 
+    where: {oauthId : profile.id.toString(), provider:profile.provider},
+  });
+
+  if (user !== null) {
+    return { id: user.id, nickname: user.nickname, account_id : user.account_id };
+  }
+
+  // 사용자가 없으면 회원가입 페이지로 이동하도록 응답
+  // ex. 프론트에서 signupRequired : true를 응답받으면 회원가입 페이지로 이동 
+  return {
+    signupRequired : true, 
+    provider : profile.provider, 
+    oauth_id : profile.id,
+  };
+};
