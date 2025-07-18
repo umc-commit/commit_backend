15fa19354541b34ce2ba3b2362178e88f509ce71
taeyoung0524 <miseoseo1@naver.com>
2025-07-18 22:00:29 +0900

    FEAT : 트위터 auth.config.js 코드 추가

--- src/auth.config.js
+++ src/auth.config.js
@@ -3,6 +3,8 @@ import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import { prisma } from "./db.config.js";
 import { Strategy as KakaoStrategy } from "passport-kakao";
 import { Strategy as NaverStrategy } from "passport-naver";
+import { Strategy as TwitterStrategy} from "passport-twitter";
+
 dotenv.config();
 
 export const googleStrategy = new GoogleStrategy(
@@ -90,6 +92,41 @@ export const naverStrategy = new NaverStrategy(
 );
 
 const naverVerify = async (profile) => {
+
+  const user = await prisma.account.findFirst({ 
+    where: {oauthId : profile.id.toString(), provider:profile.provider},
+  });
+
+  if (user !== null) {
+    return { id: user.id, nickname: user.nickname, account_id : user.account_id };
+  }
+
+  // 사용자가 없으면 회원가입 페이지로 이동하도록 응답
+  // ex. 프론트에서 signupRequired : true를 응답받으면 회원가입 페이지로 이동 
+  return {
+    signupRequired : true, 
+    provider : profile.provider, 
+    oauth_id : profile.id,
+  };
+};
+
+
+export const twittertrategy = new TwitterStrategy(
+  {
+    clientID: process.env.PASSPORT_TWITTER_CLIENT_ID,
+    clientSecret: process.env.PASSPORT_TWITTER_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/api/users/oauth2/callback/twitter",
+    scope: undefined,
+    state: true,
+  },
+  (accessToken, refreshToken, profile, cb) => {
+    return twitterVerify(profile)
+      .then((user) => cb(null, user))
+      .catch((err) => cb(err));
+  }
+);
+
+const twitterVerify = async (profile) => {
 
   const user = await prisma.account.findFirst({ 
     where: {oauthId : profile.id.toString(), provider:profile.provider},
