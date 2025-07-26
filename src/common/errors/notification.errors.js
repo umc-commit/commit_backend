8be4fabfa5ba5f32c0f100d08a301c5adea9d25c
modzivv <vvgkxm@gmail.com>
2025-07-26 14:17:46 +0900

    refactor: 알림 에러 클래스 팀 컨벤션에 맞게 리팩토링 #48

--- src/common/errors/notification.errors.js
+++ src/common/errors/notification.errors.js
@@ -1,78 +1,78 @@
 import { BaseError } from './BaseError.js';
 
 export class NotificationNotFoundError extends BaseError {
-    constructor(notificationId) {
+    constructor(data = null) {
         super({
             errorCode: "N001",
             reason: "알림을 찾을 수 없습니다",
             statusCode: 404,
-            data: { notificationId }
+            data
         });
     }
 }
 
 export class NotificationPermissionDeniedError extends BaseError {
-    constructor(userId, notificationId) {
+    constructor(data = null) {
         super({
             errorCode: "N002",
             reason: "알림에 접근할 권한이 없습니다",
             statusCode: 403,
-            data: { userId, notificationId }
+            data
         });
     }
 }
 
 export class AlreadyReadNotificationError extends BaseError {
-    constructor(notificationId) {
+    constructor(data = null) {
         super({
             errorCode: "N003",
             reason: "이미 읽은 알림입니다",
             statusCode: 400,
-            data: { notificationId }
+            data
         });
     }
 }
 
 export class NotificationSendFailedError extends BaseError {
-    constructor(reason) {
+    constructor(data = null) {
         super({
             errorCode: "N004",
             reason: "알림 발송에 실패했습니다",
             statusCode: 500,
-            data: { reason }
+            data
         });
     }
 }
 
 export class NoUnreadNotificationsError extends BaseError {
-    constructor(userId) {
+    constructor(data = null) {
         super({
             errorCode: "N005",
             reason: "읽지 않은 알림이 없습니다",
             statusCode: 400,
-            data: { userId }
+            data
         });
     }
 }
 
 export class NoNotificationsToDeleteError extends BaseError {
-    constructor() {
+    constructor(data = null) {
         super({
             errorCode: "N006",
             reason: "삭제할 알림이 없습니다",
             statusCode: 400,
-            data: {}
+            data
         });
     }
 }
 
 export class NoNotificationsSelectedError extends BaseError {
-    constructor() {
+    constructor(data = null) {
         super({
             errorCode: "N007",
             reason: "삭제할 알림을 선택해주세요",
             statusCode: 400,
-            data: { notification_ids: [] }
+            data
         });
     }
 }
