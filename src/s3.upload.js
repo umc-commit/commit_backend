e3d09004b32de7ccd71b29df40e531b56932dcf6
modzivv <vvgkxm@gmail.com>
2025-08-09 16:50:25 +0900

    feat: S3 파일 삭제 유틸리티 함수(deleteFromS3) 추가 #115

--- src/s3.upload.js
+++ src/s3.upload.js
@@ -1,4 +1,4 @@
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { S3Client, PutObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { v4 as uuidv4 } from "uuid";
 import dotenv from 'dotenv';
 
@@ -27,4 +27,23 @@ export const uploadToS3 = async (buffer, folderName = "uploads", extension = "pn
   await s3.send(command);
 
   return `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${filename}`;
+};
+
+export const deleteFromS3 = async (imageUrl) => {
+  try {
+    // URL에서 키 추출
+    const urlParts = imageUrl.split('/');
+    const key = urlParts.slice(3).join('/'); // bucket-name.s3.region.amazonaws.com/ 이후 부분
+
+    const command = new DeleteObjectCommand({
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: key,
+    });
+
+    await s3.send(command);
+    return true;
+  } catch (error) {
+    console.error('S3 파일 삭제 실패:', error);
+    throw error;
+  }
 };
